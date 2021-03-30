9b10a795da1a72529a5f09998dc059f2c97791e8
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-30 14:50:19 -0300

    Add project files.

---
+++ FechasEjercicio23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechasEjercicio23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha;
+            Console.WriteLine("Ingrese una fecha con el formato dia/mes/año");
+            if (!DateTime.TryParse(Console.ReadLine(), out fecha))
+            {
+                Console.WriteLine("Eso no es una fecha");
+            }
+            else
+            {
+                Console.WriteLine($"La fecha ingresada es {fecha}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
